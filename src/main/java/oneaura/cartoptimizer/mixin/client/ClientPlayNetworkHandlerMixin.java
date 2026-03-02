2f61fe5f0273ff5b8aab058d21c20873198a73f5
oneauraaaaa <selimemrebayar2014@gmail.com>
2026-03-02 18:51:06 +0300

    chore: remove server blacklist & bump version to 1.0.4

--- src/main/java/oneaura/cartoptimizer/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/oneaura/cartoptimizer/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -1,9 +1,6 @@
 package oneaura.cartoptimizer.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ServerInfo;
-import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,30 +12,6 @@ import org.slf4j.LoggerFactory;
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
     private static final Logger LOGGER = LoggerFactory.getLogger("oneauras-cart-optimizer-cpnh");
-
-    @Inject(method = "onGameJoin", at = @At("TAIL"))
-    private void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
-        ServerInfo serverInfo = MinecraftClient.getInstance().getCurrentServerEntry();
-        if (serverInfo != null) {
-            String serverAddress = serverInfo.address;
-            oneaura.cartoptimizer.ServerBlacklist.getInstance().setCurrentServer(serverAddress);
-            LOGGER.info("[Cart Optimizer] Joined server: " + serverAddress);
-
-            if (oneaura.cartoptimizer.ServerBlacklist.getInstance().isOnBlacklistedServer()) {
-                LOGGER.info("[Cart Optimizer] Server is blacklisted - mod disabled.");
-
-                // Send chat message to player
-                net.minecraft.client.network.ClientPlayerEntity player = MinecraftClient.getInstance().player;
-                if (player != null) {
-                    player.sendMessage(
-                            net.minecraft.text.Text.of("§c[Cart Optimizer] Cart Optimizer is disabled on this server."),
-                            false);
-                }
-            }
-        } else {
-            oneaura.cartoptimizer.ServerBlacklist.getInstance().clearServer();
-        }
-    }
 
     @Inject(method = "onScreenHandlerSlotUpdate", at = @At("HEAD"), cancellable = true)
     private void onScreenHandlerSlotUpdate(ScreenHandlerSlotUpdateS2CPacket packet, CallbackInfo ci) {
